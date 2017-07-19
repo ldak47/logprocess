6bafe07bb4f471bff4d8fd9dc823c850a9eb849a
root <root@cp01-misheng-glb-test01.epc.baidu.com>
2017-07-19 14:29:30 +0800

    add rocksdb mergeoperator

--- data-access/common/rocksdb.h
+++ data-access/common/rocksdb.h
@@ -3,14 +3,17 @@
 #include "rocksdb/db.h"
 #include "rocksdb/slice.h"
 #include "rocksdb/options.h"
+#include "rocksdb/merge_operator.h"
+#include "rocksdb/compaction_filter.h"
 #include <vector>
 #include <mutex>
+#include "glog/logging.h"
 
 namespace common {
 
 struct WriteBatcher {
     rocksdb::WriteBatch batch;
-    void add_action(std::string action, std::initializer_list<std::string> args, std::string &err, rocksdb::ColumnFamilyHandle *cf = nullptr){
+    void add_action(std::string action, std::vector<std::string> args, std::string &err, rocksdb::ColumnFamilyHandle *cf = nullptr){
         err = "";
         if (action == "clear") {
             batch.Clear();
@@ -19,7 +22,7 @@ struct WriteBatcher {
         if (action == "delete") {
             std::string key;
             try {
-                std::initializer_list<std::string>::iterator it = args.begin();
+                std::vector<std::string>::iterator it = args.begin();
                 key = *it;
             } catch (std::exception &e) {
                 err = e.what();
@@ -31,7 +34,7 @@ struct WriteBatcher {
         if (action == "put") {
             std::string key, value;
             try {
-                std::initializer_list<std::string>::iterator itk = args.begin(), itv = itk + 1;
+                std::vector<std::string>::iterator itk = args.begin(), itv = itk + 1;
                 key = *itk;
                 value = *itv;
             } catch (std::exception &e) {
@@ -44,7 +47,7 @@ struct WriteBatcher {
         if (action == "deleterange") {
             std::string key_start, key_end;
             try {
-                std::initializer_list<std::string>::iterator itks = args.begin(), itke = itks + 1;
+                std::vector<std::string>::iterator itks = args.begin(), itke = itks + 1;
                 key_start = *itks;
                 key_end = *itke;
             } catch (std::exception &e) {
@@ -57,7 +60,7 @@ struct WriteBatcher {
         if (action == "merge") {
             std::string key, value;
             try {
-                std::initializer_list<std::string>::iterator itk = args.begin(), itv = itk + 1;
+                std::vector<std::string>::iterator itk = args.begin(), itv = itk + 1;
                 key = *itk;
                 value = *itv;
             } catch (std::exception &e) {
@@ -73,17 +76,41 @@ struct WriteBatcher {
     }
 };
 
+class ProcessMerge : public rocksdb::MergeOperator {
+public:
+    virtual bool FullMergeV2 (const MergeOperationInput &merge_in,
+                              MergeOperationOutput *merge_out) const override {
+        merge_out->new_value.clear();
+        if (merge_in.existing_value != nullptr) {
+            merge_out->new_value.assign(merge_in.existing_value->data(), merge_in.existing_value->size());
+        }
+
+        for (const rocksdb::Slice& m : merge_in.operand_list) {
+            if (merge_out->new_value != "") {
+                merge_out->new_value.append("|");
+            }
+            merge_out->new_value.append(m.data(), m.size());
+        }
+
+        return true;
+    }
+
+    const char* Name() const override {
+        return "ProcessMerge";
+    }
+};
+
 class Rocksdb {
 public:
     rocksdb::DB *db_;
     rocksdb::Options options_;
-    void default_setup(const char *dir, std::initializer_list<std::string> columnfamily);
+    void default_setup(const char *dir, std::vector<std::string> columnfamily);
 
     std::vector<rocksdb::ColumnFamilyHandle *> cfs_;
     std::map<std::string, rocksdb::ColumnFamilyHandle *> cfmap_;
     
     int err_flag_;
-    Rocksdb(const char *dir, std::initializer_list<std::string> columnfamily = {});
+    Rocksdb(const char *dir, std::vector<std::string> columnfamily = {});
     ~Rocksdb();
     
     bool Put(const std::string key, const std::string value, std::string cf = "");
@@ -94,7 +121,7 @@ public:
     bool DeleteRange(const std::string key_start, const std::string key_end, std::string cf = "");
 
     WriteBatcher *BatchInit();
-    void BacthAddAction(WriteBatcher *handle, std::string action, std::initializer_list<std::string> args, std::string &err, rocksdb::ColumnFamilyHandle *cf = nullptr);
+    void BacthAddAction(WriteBatcher *handle, std::string action, std::vector<std::string> args, std::string &err, rocksdb::ColumnFamilyHandle *cf = nullptr);
     bool BatchDoAction(WriteBatcher *handle);
 
     bool CreateColumnFamily(const std::string cf);
